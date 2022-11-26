2ebe32ca0eb69741d8d44f3979bd478a3a56b0f1
DRILON REXHEPI <drilonrexhepi98@gamil.com>
2022-11-26 13:16:55 +0100

    API skeleton

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public String UserName { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+    }
+}  
---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+//pjesa lart Builder jane service, dhe nuk ndikojne direkt ne app
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Run();
