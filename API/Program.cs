a9cd239f3b975c4aecc55adcbf65a70b9585aae2
DRILON REXHEPI <drilonrexhepi98@gamil.com>
2022-11-28 00:25:42 +0100

    Angular skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 //pjesa lart Builder jane service, dhe nuk ndikojne direkt ne app
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
